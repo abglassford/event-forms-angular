c3e84836f2558688ffefe6bfe942598a8f19304e
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-12 16:11:01 -0600

    add ping pong score counter

--- controllers.js
+++ controllers.js
@@ -4,9 +4,26 @@
   const app = angular.module('exercisesApp');
 
   app.controller('reverseController', function () {
+
     this.reverseString = function (input) {
       let newString = input.split('').reverse().join('')
-      this.reversedString = newString
+      this.input = newString
     }
   })
+
+  app.controller('pingPongController', function () {
+
+    this.player1 = {}
+    this.player2 = {}
+
+    this.player1.score = 0;
+    this.player2.score = 0;
+
+    this.addPoint = function (player) {
+      if (player.score < 11 ) {
+        player.score++
+      }
+    }
+  })
+
 }());
