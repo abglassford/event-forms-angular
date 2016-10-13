223101e91621564bf1ab5d844609cc96ed80fa80
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-13 00:17:07 -0600

    change ping pong exercise so that ever other game, the player who serves is switched

--- controllers.js
+++ controllers.js
@@ -32,11 +32,13 @@
       losses: 0
     }
     this.game = {
+      number: 1,
       point: 0
     }
     this.play = function (player) {
       const p1 = this.player1;
       const p2 = this.player2;
+
       if (p1.winner === false && p2.winner === false) {
         this.game.point++
         player.score++
@@ -49,24 +51,28 @@
           p2.loser = true
           p1.wins++
           p2.losses++
+          this.game.number++
         } else if (p2.score >= 11 && p1.score < p2.score - 1) {
           p2.winner = true
           p1.loser = true
           p2.wins++
           p1.losses++
+          this.game.number++
         }
       }
     }
     this.reset = function () {
       this.player1.score = 0
-      this.player1.serving = true
       this.player1.winner = false
       this.player1.loser = false
       this.player2.score = 0
-      this.player2.serving = false
       this.player2.winner = false
       this.player2.loser = false
       this.game.point = 0
+      if (this.game.number % 2 === 0) {
+        this.player1.serving = false
+        this.player2.serving = true
+      }
     }
   }
 
@@ -86,15 +92,25 @@
         var color = "#" + z1 + x;
         return color;
       }
+
+      var replaying = false;
+
+      this.replay = function() {
+        var displayPrevColor = function() {
+          // do some logic to change color
+          // if done replay colors
+          replaying = false;
+          // else
+          $timeout(displayPrevColor, 1000);
+          // end if/else
+        };
+        if (!replaying) {
+          replaying = true;
+          // This timeout starts the timeout loop
+          $timeout(function() { displayPrevColor(); }, 500);
+        }
+      };
   }
 
-  function randomColor() {
-    var x = Math.round(0xffffff * Math.random()).toString(16);
-    var y = (6-x.length);
-    var z = "000000";
-    var z1 = z.substring(0,y);
-    var color = "#" + z1 + x;
-    return color;
-  }
 
 }());
